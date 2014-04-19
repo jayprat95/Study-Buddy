81ca94bedeed771c31667389ca4b4a86a1ee3518
C-Nolan <christophermichaelnolan@gmail.com>
2014-04-19 02:20:52 -0400

    Extra view

---
+++ Study-Buddy/SBHomePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBHomePageViewController.h
+//  
+//
+//  Created by Christopher Nolan on 4/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBHomePageViewController : UIViewController
+
+@end
