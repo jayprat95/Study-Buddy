bbe32b64f49598cca79cef2cd64a7480eaee459a
C-Nolan <christophermichaelnolan@gmail.com>
2014-04-19 00:28:17 -0400

    textFields

---
+++ Study-Buddy/Study Buddy/SBFirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  SBFirstViewController.m
+//  Study Buddy
+//
+//  Created by Jayanth Prathipati on 4/18/14.
+//  Copyright (c) 2014 VTHacksProject. All rights reserved.
+//
+
+#import "SBFirstViewController.h"
+
+@interface SBFirstViewController ()
+
+@end
+
+@implementation SBFirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Study-Buddy/Study Buddy/SBLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  SBLoginViewController.h
+//  Study Buddy
+//
+//  Created by Christopher Nolan on 4/19/14.
+//  Copyright (c) 2014 VTHacksProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBLoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *pwdTextField;
+
+@end
