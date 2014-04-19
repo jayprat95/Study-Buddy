fdb66a2cd723eb0b419dbbc4e849ec7d6b517ae0
Jayanth Prathipati <jayanth@vt.edu>
2014-04-18 23:06:23 -0400

    Initial Commit
    Adds Initial XCodeProj

---
+++ Study Buddy/SBSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBSecondViewController.h
+//  Study Buddy
+//
+//  Created by Jayanth Prathipati on 4/18/14.
+//  Copyright (c) 2014 VTHacksProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBSecondViewController : UIViewController
+
+@end
